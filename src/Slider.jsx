145298b49417d9075dcdb687a321c50736174746
ValtierraXavier <xavier.valtierra@icloud.com>
2023-01-14 12:54:56 -0500

    Final Commit

--- src/Slider.jsx
+++ src/Slider.jsx
@@ -9,6 +9,10 @@ function Slider(){
 
     const pageUp = () => setPageNumber(prev => prev + 1)
     const pageDown = () => setPageNumber(prev => prev - 1)
+    const fiftyUp = () => setPageNumber(prev => prev + 50)
+    const fiftyDown = () => setPageNumber(prev => prev -50)
+    const begining = () => setPageNumber(prev => prev = 1)
+    const end = () => setPageNumber(prev => prev = 826)
     const theImage = `https://rickandmortyapi.com/api/character/avatar/${pageNumber}.jpeg`
 
     const fetchCharacter = async () => {
@@ -16,23 +20,49 @@ function Slider(){
         setCharacter(response.data)
     }
     
+    function limiter(){
+        if(pageNumber<1){
+            setPageNumber(prev => prev = 1)
+        } else if(pageNumber > 826){
+            setPageNumber(prev => prev = 826)
+        }
+    }
+
     useEffect(()=>{
         fetchCharacter()
+        limiter()
     }, [pageNumber])
 
 return (
     <div className ='infoWindow'>  
-        <button disabled={pageNumber >= 826} className ='next' onClick={pageUp}>▶︎</button>
+        
+        <div className="forwardButtons">
+            <button disabled={pageNumber >= 826} className ='next' onClick={pageUp}>▶︎</button>
+            <button disabled={pageNumber >= 826} className ='fiftyUp' onClick={fiftyUp}>▷▶</button>
+            <button disabled={pageNumber >= 826} className ='end' onClick={end}>⇥</button>
+        </div>
+
         <div className = 'display'>
+
             <div className ='imageDiv'>
                 <img className ='image' src={theImage} alt='Character Image'></img>
             </div>
+
             <div className='textArea'>
                 <h3 className="name">Name: {character.name}</h3>
                 <h3 className="status">Status: {character.status}</h3>
+                <p className="outOf">{pageNumber} of 826</p>
             </div>
+
         </div>
-    <button disabled={pageNumber <= 1} className ='prev' onClick={pageDown}>◀︎</button>
-  </div>)
+
+    <div className='backButtons'>
+        <button disabled={pageNumber <= 1} className ='prev' onClick={pageDown}>◀︎</button>
+        <button disabled={pageNumber <= 1} className ='fiftyDown' onClick={fiftyDown}>◀︎◁</button>
+        <button disabled={pageNumber <= 1} className ='start' onClick={begining}>⇤</button>
+    </div>
+
+  </div> 
+  )
 } 
 export default Slider
